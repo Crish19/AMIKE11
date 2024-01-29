de29997611257916336238a2ffe8a11238bf96c2
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2024-01-29 11:32:46 -0300

    routes

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(
-  "/public/uploads",
-  express.static(path.join(__dirname, "public/uploads"))
-);
+app.use(express.static(path.join(__dirname, "public")));
 
 require("dotenv").config();
 
